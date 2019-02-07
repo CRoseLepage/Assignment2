a1c7941812921e80bd33a9399cf0701d3fae900a
Chantel Lepage <chantellepage@AD.DOE.CARLETON.CA>
2019-02-07 15:50:23 -0500

    First upload. Not completed

---
+++ assign_2_finite_difference_method.m
@@ -0,0 +1,23 @@
+% Assignment 2 Finite Difference Method
+% Chantel Lepage 100999893
+
+%%Part 1
+
+L=50;
+W=50;
+
+G=sparse(L,W);
+E=zeros(1,L);
+
+for i=1:L
+    
+    
+    
+    
+end
+
+
+
+
+
+
